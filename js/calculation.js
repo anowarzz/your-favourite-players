9bf84473b153e6e44929e1b6cf92e35d78098186
anowarzz <anowarhosen444@gmail.com>
2022-08-22 20:20:37 +0600

    calculate total button works now

--- js/calculation.js
+++ js/calculation.js
@@ -1,18 +1,42 @@
-// Total Expense calculation
 
 
-//  Adding event listener to Calculate total button.
-document.getElementById("calculate-total-btn").addEventListener("click", function(){
-
-    // Getting value from Manager Expense Field.
-    getInputFieldValueById("manager-expense");
-
-    // Getting value from coach expense field.
-    getInputFieldValueById("coach-expense");
-    console.log();
-    
-    
 
 
+
+
+
+
+// Player Expennses Calculation.
+
+// Adding event listener to the  calculate button.
+document.getElementById("calculate-btn").addEventListener("click", function(){
+
+    // setting All player expense to display
+    setTextElementValueById("player-total-expense", playerTotalExpense())
 })
 
+
+
+
+
+
+
+// Total Expense calculation  ------>
+
+//  Adding event listener to Calculate total button.
+document.getElementById("calculate-total-btn")
+  .addEventListener("click", function () {
+
+    // Getting value from Manager Expense Field.
+    const managerExpense = getInputFieldValueById("manager-expense-field");
+
+    // Getting value from coach expense field.
+    const coachExpense = getInputFieldValueById("coach-expense-field");
+
+
+    // Total expense calculation.
+    const totalExpense = playerTotalExpense() + managerExpense + coachExpense;
+
+    // Setting Final total expense amount to display
+    setTextElementValueById("all-total-expense", totalExpense);
+  });
--- js/common-functions.js
+++ js/common-functions.js
@@ -1,26 +1,31 @@
-// 1. To get value from input field .
-
+// 1. To get value from input field  ---------->
 function getInputFieldValueById(inputFieldId) {
-    const inputField = document.getElementById(inputFieldId);
-    const inputFieldValueString = inputField.value;
-    const inputFieldAmount = parseFloat(inputFieldValueString);
-    return inputFieldAmount;
-    inputField.value = "";
-  }
-  
-  // 2. To get value from Text element.
-  
-  function getTextElementValueById(elementId) {
-    const textElement = document.getElementById(elementId);
-    const textElementValueString = textElement.value;
-    const textElementValue = parseFloat(textElementValueString);
-    return textElementValue;
-  }
-  
-  // 2. To set new value in text element .
-  
-  function setTextElementValueById(elementId, newValue) {
-    const textElement = document.getElementById(elementId);
-    textElement.innerText = newValue;
-  }
-  
+  const inputField = document.getElementById(inputFieldId);
+  const inputFieldValueString = inputField.value;
+  const inputFieldAmount = parseFloat(inputFieldValueString);
+  return inputFieldAmount;
+  inputField.value = "";
+}
+
+// 2. To get value from Text element ------->
+function getTextElementValueById(elementId) {
+  const textElement = document.getElementById(elementId);
+  const textElementValueString = textElement.value;
+  const textElementValue = parseFloat(textElementValueString);
+  return textElementValue;
+}
+
+// 3. To set new value in text element --------->
+
+function setTextElementValueById(elementId, newValue) {
+  const textElement = document.getElementById(elementId);
+  textElement.innerText = newValue;
+}
+
+// 4. Calculating all player expense together ---->
+
+function playerTotalExpense() {
+  const perPlayerExpense = getInputFieldValueById("per-player-expense-field");
+  const totalPlayerExpense = perPlayerExpense * 5;
+  return totalPlayerExpense;
+}
